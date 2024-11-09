cd14fe623cfaed98d9d9fb3d673b490682010459
Daye Sow <daye2001@live.fr>
2024-11-09 22:47:01 +0100

    video page fix

--- src/components/Pages/WipPage.jsx
+++ src/components/Pages/WipPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import gsap from "gsap";
 import "./wipPage.scss";
 import Marquee from "../marquee/Marquee";
@@ -8,33 +8,34 @@ const WipPage = () => {
   const handleNavigation = useHandleNavigation();
   const galleryRef = useRef(null);
 
-  const images = [
-    "https://victorverheyden.com/wp-content/uploads/2023/11/IgorDieryck-1-Medium.jpeg",
-    "https://victorverheyden.com/wp-content/uploads/2023/11/Rodrigo-1-Medium.jpeg",
-    "https://victorverheyden.com/wp-content/uploads/2023/11/wip-Medium.jpeg",
-    "https://victorverheyden.com/wp-content/uploads/2023/11/jaouad-1-Medium.jpeg",
-    "https://victorverheyden.com/wp-content/uploads/2023/11/photobook-1-Medium.jpeg",
-    "https://victorverheyden.com/wp-content/uploads/2023/11/MG_9917-min-scaled.jpg",
-    "https://victorverheyden.com/wp-content/uploads/2023/11/MG_8465finaal2-min-scaled.jpg",
-    "https://victorverheyden.com/wp-content/uploads/2023/11/MG_7778reversed-bnw-min-scaled.jpg",
-  ];
+  const images = useMemo(
+    () => [
+      "https://victorverheyden.com/wp-content/uploads/2023/11/IgorDieryck-1-Medium.jpeg",
+      "https://victorverheyden.com/wp-content/uploads/2023/11/Rodrigo-1-Medium.jpeg",
+      "https://victorverheyden.com/wp-content/uploads/2023/11/wip-Medium.jpeg",
+      "https://victorverheyden.com/wp-content/uploads/2023/11/jaouad-1-Medium.jpeg",
+      "https://victorverheyden.com/wp-content/uploads/2023/11/photobook-1-Medium.jpeg",
+      "https://victorverheyden.com/wp-content/uploads/2023/11/MG_9917-min-scaled.jpg",
+      "https://victorverheyden.com/wp-content/uploads/2023/11/MG_8465finaal2-min-scaled.jpg",
+      // "https://victorverheyden.com/wp-content/uploads/2023/11/MG_7778reversed-bnw-min-scaled.jpg",
+    ],
+    []
+  );
 
   useEffect(() => {
-    // Wait until images are loaded and gallery is rendered
     const galleryItems = galleryRef.current.querySelectorAll(".marquee__item");
     if (galleryItems.length === 0) return;
 
-    const numImages = galleryItems.length;
-    const totalWidth = galleryItems[0].offsetWidth * numImages;
+    const itemWidth = galleryItems[0].offsetWidth;
+    const totalWidth = itemWidth * galleryItems.length;
 
-    // Define infinite scroll animation with GSAP
     gsap.to(galleryRef.current, {
-      x: `-=${totalWidth}px`, // Move the gallery to the left
-      duration: numImages * 5, // Adjusts speed based on the number of images
+      x: `-=${totalWidth / 2}px`, // verschuif de galerie naar links
+      duration: galleryItems.length * 1.5, // snelheid aanpassen op basis van aantal afbeeldingen
       ease: "linear",
-      repeat: -1, // Infinite loop
+      repeat: -1, // oneindige loop
       modifiers: {
-        x: (x) => `${parseFloat(x) % totalWidth}px`, // Ensures the loop is seamless
+        x: (x) => `${parseFloat(x) % (totalWidth / 2)}px`, // naadloze loop
       },
     });
   }, [images, galleryRef]);
@@ -43,7 +44,7 @@ const WipPage = () => {
     <>
       <b> So Close Yet So Far From Paradise </b> - Work in progress - Unfolded
       during Covid, reflecting on emotions, freedom, and the endless search for
-      paradise -<b> So Close Yet So Far From Paradise </b> - Work in progress -
+      paradise - <b> So Close Yet So Far From Paradise </b> - Work in progress -
       Unfolded during Covid, reflecting on emotions, freedom and the endless
       search for paradise -
     </>
@@ -52,12 +53,9 @@ const WipPage = () => {
   return (
     <div className="wippage-section">
       <Marquee text={marqueeText} duration={40} />
-      <div className="gallery" ref={galleryRef}>
-        {/* Map the images twice to create a seamless loop */}
-        {images
-          .concat(images)
-
-          .map((image, index) => (
+      <div className="wrapper">
+        <div className="gallery" ref={galleryRef}>
+          {images.concat(images).map((image, index) => (
             <img
               className="marquee__item"
               key={index}
@@ -66,6 +64,7 @@ const WipPage = () => {
               loading="lazy"
             />
           ))}
+        </div>
       </div>
       <a
         className="next-btn"
